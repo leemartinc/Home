38ae1870d65b9b2c17c4366c4e2d8c16f8da72d5
Lee-Martin Clarke <mobeel5356@gmail.com>
2016-07-17 13:59:10 -0400

    Mach 1

---
+++ swag.java
@@ -0,0 +1,12 @@
+/**
+ * Created by Lee-Martin Clarke on 7/14/2016.
+ */
+public class swag {
+
+    public static void main (String[] args){
+
+        System.out.println("you da rock");
+
+    }
+
+}
